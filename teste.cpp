4f9f68b218b8b5155a659c35a625b5a8107aa672
theeam <eduuardomuller@gmail.com>
2017-12-05 11:33:23 -0300

    teste2

--- teste.cpp
+++ teste.cpp
@@ -32,12 +32,24 @@ bool intersect(const vec& origin, const vec& direction, const vec& center, doubl
 struct Material {
 	vec kd;
 	vec ke;
+
 	double shininess;
+	double reflection;
 
 	Material& operator = (const Material& mat) {
 		ke = mat.ke;
 		kd = mat.kd;
 		shininess = mat.shininess;
+		reflection = mat.reflection;
+		return *this;
+	}
+};
+
+struct Ambient {
+	vec ka;
+
+	Ambient& operator = (const Ambient& amb) {
+		ka = amb.ka;
 		return *this;
 	}
 };
@@ -207,6 +219,7 @@ vector<Object*> loadObject(const string nomeArq, Material materialTriangle) { //
 	Vertice v;
 	std::vector<Object*> objetos;
 	mat rotY, rotX;
+	float mult_coord, somar_z;
 	double sent = 0.707106; //seno de 45 graus
 	double cost = 0.707106; //cos de 45 graus
 	double senj = -0.5; //seno de 30 graus
@@ -233,63 +246,45 @@ vector<Object*> loadObject(const string nomeArq, Material materialTriangle) { //
 				continue;
 			}
 			else
-				if (type == "v") {
-					float x, y, z;
-					stream >> x >> y >> z; //pega as coordenadas x,y,z do arquivo
-					v.coordenada << x << y << z; //cria o vertice com as coordenadas
-
-					v.coordenada = rotY * v.coordenada; //multiplica as coordenadas pela matriz de rotacao em torn ode Y
-					v.coordenada = rotX * v.coordenada; //multiplica as coordenadas pela matriz de rotacao em torn ode X
-					v.coordenada.at(2) = v.coordenada.at(2) + 10.0;
-					vertices.push_back(v); //adiciona o vertice ao vector
-
+				if (type == "m") {
+					stream >> mult_coord; //valor para multiplicar todas as coordenadas
 				}
 				else
-					if (type == "vn") {
-						float t, r, s;
-						stream >> t >> r >> s;
-						continue;
-
+					if (type == "z") {
+						stream >> somar_z; // valor para somar na coordenada z
 					}
 					else
-						if (type == "f") {
+						if (type == "v") {
+							float x, y, z;
+							stream >> x >> y >> z; //pega as coordenadas x,y,z do arquivo
+							v.coordenada << x << y << z; //cria o vertice com as coordenadas
 
-							/*int numDeBarras = std::count(line.begin(), line.end(), '/');
-							char c = '/';
-							cout  << line.find(c) << numDeBarras;
-							cin.get();
-
-							if (numDeBarras == 0) {
-							stream >> f >> g >> h;
-							}
-							else if (numDeBarras == 3) {
-							int barraN1 = findNext(line, 0, '/' );
-							int barraN2 = findNext(line, barraN1, '/');
-							f = stoi(line.substr(0, barraN1)); //pega a o primeiro vertice da face
-							g = stoi(line.substr((barraN1+1), barraN2));
-							h = stoi(line.substr((barraN2), line.length()));
-							//Ta tudo errado
-
-							}
-							else if (numDeBarras == 6) {
-
-							}*/
-
-
-							int f, g, h;
-							stream >> f >> g >> h;
-
-							face.a = (f - 1);
-							face.b = (g - 1);//cria a face com os vertices
-							face.c = (h - 1);
-
-							faces.push_back(face);
-
+							v.coordenada = rotY * v.coordenada; //multiplica as coordenadas pela matriz de rotacao em torn ode Y
+							v.coordenada = rotX * v.coordenada; //multiplica as coordenadas pela matriz de rotacao em torn ode X
+							v.coordenada.at(2) = v.coordenada.at(2) + somar_z; // soma um valor a coordenada z para que possa ser vista
+							v.coordenada *= mult_coord; //multiplica todas as coordenadas por um valor, para aumentar ou diminuir o tamanho da img
+							vertices.push_back(v); //adiciona o vertice ao vector
 
 						}
-						else {
-							//não faz nada
-						}
+						else
+							if (type == "vn") {
+								float t, r, s;
+								stream >> t >> r >> s;
+								continue;
+
+							}
+							else
+								if (type == "f") {
+									int f, g, h;
+									stream >> f >> g >> h;
+									face.a = (f - 1);
+									face.b = (g - 1);//cria a face com os vertices
+									face.c = (h - 1);
+									faces.push_back(face);
+								}
+								else {
+									//não faz nada
+								}
 
 
 		}
@@ -306,14 +301,6 @@ vector<Object*> loadObject(const string nomeArq, Material materialTriangle) { //
 			v2 = vertices[f2].coordenada;
 			v3 = vertices[f1].coordenada;
 
-			cout << "f1 - " << f1 << endl;
-			cout << "f2 - " << f2 << endl;
-			cout << "f3 - " << f3 << endl;
-
-			cout << "v1 - " << v1 << endl;
-			cout << "v2 - " << v2 << endl;
-			cout << "v3 - " << v3 << endl;
-
 
 			objetos.push_back(new Triangle(v1, v2, v3, materialTriangle)); //adiciona o objeto ao vetor
 		}
@@ -323,32 +310,101 @@ vector<Object*> loadObject(const string nomeArq, Material materialTriangle) { //
 
 
 	myfile.close();
-	//cin.get();
 	return objetos;
+}
+
+vec rayCast(Ray r, std::vector <Object*> objetos, std::vector<Luz> luzes, int salto) {
+	vec cor;
+	vec corResto;
+	cor << 0.0 << 0.0 << 0.0;
+	corResto << 127.0 << 127.0 << 127.0;
+
+	if (salto < 3) {
+		Ambient a;
+		a.ka << 127.0 << 127.0 << 127.0;
+
+		//luz ambiente
+		double intensidade = 0.15;
+		vec posicao_luz;
+		posicao_luz << 0.0 << 0.0 << -2.0;
+		vec radiancia_luz;
+		radiancia_luz << intensidade << intensidade << intensidade;
+		Luz luzA;
+		luzA.pos = posicao_luz;
+		luzA.radiancia = radiancia_luz;
+
+		bool intersected = false;
+		Intersection da;
+		Intersection shadow;
+
+		/*
+		l = pos_luz - p;
+		Intersection shadow;
+		Ray r2(da.p,l);
+		for(int o = 0;o<objetos.size();++o){
+		if (!(objetos[o]->intersect(r2,&shadow) && shadow.t>0.0 && shadow.t <1.0;
+		cor+= blinn phong
+		}
+		*/
+
+		for (int i = 0; i < objetos.size(); ++i) {
+			Intersection da_temp;
+			if (objetos[i]->intersect(r, da_temp)) {
+				if (intersected && da_temp.t < da.t) {
+					da = da_temp;
+				}
+				else if (!intersected) {
+					intersected = true;
+					da = da_temp;
+				}
+			}
+		}
+
+		if (intersected) {
+			vec k;
+			k << 0.0 << 0.0 << 0.0;
+
+			//Intersection reflect;
+			//rLinhaDir /= norm(rLinhaDir);
+			//reflect.normal /= norm(reflect.normal);
+
+			for (int li = 0; li < luzes.size(); ++li) {
+				Luz luz_atual = luzes[li];
+				vec l = luz_atual.pos - da.p;
+				l /= norm(l);
+				da.normal /= norm(da.normal);
+				// if (dot(l, da.normal) < 0) da.normal *= -1;  // <<serve para o fix das faces dos triangulos do cubo
+				r.direction /= norm(r.direction);
+				vec h = (-r.direction + l);
+				h /= norm(h);
+
+				Ray r2(da.p, l);
+
+				for (int o = 0; o < objetos.size(); ++o) {
+					if (!(objetos[o]->intersect(r2, shadow) && shadow.t > 0.0 && shadow.t < 1.0)) {
+						k += ((da.material.kd % luz_atual.radiancia)*std::max(0.0, dot(da.normal, l)) +
+							(da.material.ke % luz_atual.radiancia)*pow(std::max(0.0, dot(da.normal, h)), da.material.shininess));
+					}
+				}
+			}
+
+			vec rLinhaDir = (r.direction) - (2.0 * (dot(r.direction, da.normal))*da.normal);
+			Ray rLinha(da.p + da.normal*0.001, -rLinhaDir);
+			if (salto < 3) {
+				++salto;
+				cor = (1.0 - da.material.reflection)*k + (da.material.reflection*(rayCast(rLinha, objetos, luzes, salto)));
+			}
+			else {
+				cor = k;
+			}
+		}
+	}
+	return cor;
+
 }
 
 
 int main() {
-
-	double intensidade = 0.9;
-	vec posicao_luz;
-	posicao_luz << -5.0 << 8.0 << 0.0;
-	vec radiancia_luz;
-	radiancia_luz << intensidade << intensidade << intensidade;
-	Luz luz;
-	luz.pos = posicao_luz;
-	luz.radiancia = radiancia_luz;
-
-	mat k;
-	k << 1000.0 << 0.0 << 400.0 << endr
-		<< 0.0 << -1000.0 << 300.0 << endr
-		<< 0.0 << 0.0 << 1.0;
-	mat invk = k.i();
-
-	Material materialTriangle;
-	materialTriangle.kd << 255.0 << 255.0 << 255.0;
-	materialTriangle.ke << 0.0 << 0.0 << 0.0;
-	materialTriangle.shininess = 20.0;
 
 	ofstream output;
 	output.open("imagem.pgm");
@@ -356,80 +412,140 @@ int main() {
 	output << "800 600" << endl;
 	output << "255" << endl;
 
-
-
 	vec origin;
-	origin << 0.0 << 0.0 << 0.0;
+	origin << 0.0 << 0.0 << -3.0;
+
 	vec centro;
 	centro << 0.0 << 0.0 << 10.0;
+	vec centro2;
+	centro2 << 2.0 << 0.0 << 10.0;
+	vec centro3;
+	centro3 << 1.0 << 1.75 << 10.0;
 
-	std::vector<Object*> objetos;
-	objetos = loadObject("dodecahedron.txt", materialTriangle); //carregar objeto
 
+	//luz ambiente
+	double intensidade1 = 0.15;
+	vec posicao_luz1;
+	posicao_luz1 << 0.0 << 0.0 << -2.0;
+	vec radiancia_luz1;
+	radiancia_luz1 << intensidade1 << intensidade1 << intensidade1;
+	Luz luz1;
+	luz1.pos = posicao_luz1;
+	luz1.radiancia = radiancia_luz1;
+
+	double intensidade2 = 0.05;
+	vec posicao_luz2;
+	posicao_luz2 << 0.0 << 4.0 << 3.0;
+	vec radiancia_luz2;
+	radiancia_luz2 << intensidade2 << intensidade2 << intensidade2;
+	Luz luz2;
+	luz2.pos = posicao_luz2;
+	luz2.radiancia = radiancia_luz2;
+
+	double intensidade3 = 0.25;
+	vec posicao_luz3;
+	posicao_luz3 << 6.0 << 1.0 << 3.0;
+	vec radiancia_luz3;
+	radiancia_luz3 << intensidade3 << intensidade3 << intensidade3;
+	Luz luz3;
+	luz3.pos = posicao_luz3;
+	luz3.radiancia = radiancia_luz3;
+
+	double intensidade4 = 0.4;
+	vec posicao_luz4;
+	posicao_luz4 << -6.0 << 1.0 << 0.0;
+	vec radiancia_luz4;
+	radiancia_luz4 << intensidade4 << intensidade4 << intensidade4;
+	Luz luz4;
+	luz4.pos = posicao_luz4;
+	luz4.radiancia = radiancia_luz4;
+
+	std::vector <Object*> objetos;
 	std::vector<Luz> luzes;
-	luzes.push_back(luz);
 
+	luzes.push_back(luz1);
+	luzes.push_back(luz2);
+	luzes.push_back(luz3);
+	luzes.push_back(luz4);
+
+	Material material;
+	material.kd << 255.0 << 255.0 << 0.0;
+	material.ke << 255.0 << 255.0 << 255.0;
+	material.shininess = 50.0;
+	material.reflection = 0.8;
+
+	Material material2;
+	material2.kd << 255.0 << 0.0 << 0.0;
+	material2.ke << 255.0 << 255.0 << 255.0;
+	material2.shininess = 50.0;
+	material2.reflection = 0.5;
+
+	Material materialTriangle;
+	materialTriangle.kd << 0.0 << 255.0 << 0.0;
+	materialTriangle.ke << 255.0 << 255.0 << 255.0;
+	materialTriangle.reflection = 0.9;
+	materialTriangle.shininess = 35.0;
+
+	vec a1, a2, a3, a4;
+	a1 << -8.0 << -1.0 << 0.0;
+	a2 << 8.0 << -1.0 << 0.0;
+	a3 << -8.0 << -1.0 << 20.0;
+	a4 << 8.0 << -1.0 << 20.0;
+
+	Triangle t1(a1, a2, a3, materialTriangle);
+	Triangle t2(a2, a3, a4, materialTriangle);
+
+
+	objetos.push_back(new Sphere(centro, 1.0, material));
+	objetos.push_back(new Sphere(centro2, 1.0, material2));
+	objetos.push_back(new Sphere(centro3, 1.0, materialTriangle));
+	//objetos.push_back(&t1);
+	//objetos.push_back(&t2);
+
+
+	/*
+	vec posicao_luz;
+	posicao_luz << 0.0 << 0.0 << -2.0;
+	vec radiancia_luz;
+	radiancia_luz << intensidade << intensidade << intensidade;
+	Luz luzA;
+	luzA.pos = posicao_luz;
+	luzA.radiancia = radiancia_luz;
+
+
+	Ambient a;
+	a.ka << 127.0 << 127.0 << 127.0;
+	*/
+
+	mat k;
+	k << 1000.0 << 0.0 << 400.0 << endr
+		<< 0.0 << -1000.0 << 300.0 << endr
+		<< 0.0 << 0.0 << 1.0;
+	mat invk = k.i();
+
+	vec cor;
+	cor << 0.0 << 0.0 << 0.0;
+
+	//monta a janela da imagem
 	for (int linha = 0; linha < 600; ++linha) {
 		for (int coluna = 0; coluna < 800; ++coluna) {
-			bool intersected = false;
 			vec dir;
 			dir << coluna << linha << 1.0;
 			dir = invk*dir;
 			dir = (dir / dir(2))*5.0;
 			Ray r(origin, dir);
-			Intersection da;
-			for (int i = 0; i<objetos.size(); ++i) {
-				Intersection da_temp;
-				if (objetos[i]->intersect(r, da_temp)) {
-					if (intersected && da_temp.t<da.t) {
-						da = da_temp;
-					}
-					else if (!intersected) {
-						intersected = true;
-						da = da_temp;
-					}
-				}
-			}
 
-			if (intersected) {
-				vec cor;
-				cor << 0.0 << 0.0 << 0.0;
-				for (int li = 0; li < luzes.size(); ++li) {
-					Luz luz_atual = luzes[li];
-					vec l = luz_atual.pos - da.p;
+			//calcula a cor de cada ponto
+			cor = rayCast(r, objetos, luzes, 0);
 
-					da.normal /= norm(da.normal);
-					//if (da.flipNormal && dot(da.normal, l) < 0.0) da.normal *= -1.0;
-
-					Intersection shadow;
-					Ray r2(da.p, l);
-					l /= norm(l);
-					vec h = l + da.normal;
-					h /= norm(h);
-					bool paint = true;
-					for (int o = 0; o < objetos.size(); ++o) {
-						bool hit = objetos[o]->intersect(r2, shadow);
-						if (hit && (shadow.t > 0.0001) && (shadow.t < 1.0)) {
-							paint = false;
-							break;
-						}
-					}
-					if (paint) {
-						cor += (da.material.kd % luz_atual.radiancia)*std::max(0.0, dot(da.normal, l)) +
-							(da.material.ke % luz_atual.radiancia)*pow(std::max(0.0, dot(h, da.normal)), da.material.shininess);
-					}
-
-				}
-
-				output << std::min(255, int(cor(0))) << " "
-					<< std::min(255, int(cor(1))) << " "
-					<< std::min(255, int(cor(2))) << " ";
-			}
-			else {
-				output << "255 255 255 ";
-			}
+			//imprime a img
+			output << std::min(255, int(cor(0))) << " "
+				<< std::min(255, int(cor(1))) << " "
+				<< std::min(255, int(cor(2))) << " ";
 		}
 	}
+
+
 	output.close();
 	return 0;
 }
